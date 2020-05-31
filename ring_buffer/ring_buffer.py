01064deac0a66befabf0c1cdd7d5dd8e29041268
yurika sakae <yurika.sun@gmail.com>
2020-05-30 17:02:49 -0700

    ring buffer complete - all test passed

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,18 @@
 class RingBuffer:
+    # queue structure implemented using a python list
+    # oldest item leaves queue first (FIFO) when capacity is reached, and new item is inserted to the end of the queue
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        # works as a queue # has to initiate an array filled with None with size Capacity , otherwise index method will not work
+        self.storage = [None]*capacity
+        self.current_index = 0
 
     def append(self, item):
-        pass
+        self.storage[self.current_index] = item
+        self.current_index += 1
+        if self.current_index == self.capacity:
+            self.current_index = 0
 
     def get(self):
-        pass
+        filtered = [item for item in self.storage if item is not None]
+        return filtered
